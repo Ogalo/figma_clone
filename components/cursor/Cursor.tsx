70cfcf793f3ec815173eea0cfe6168721c8e8c94
Ogalo <ogaloharrison@gmail.com>
2024-05-21 23:23:52 +0300

    Add live messages

--- components/cursor/Cursor.tsx
+++ components/cursor/Cursor.tsx
@@ -13,6 +13,12 @@ const Cursor = ({ color, x, y, message}: Props) => {
       <CursorSVG color={color} />
 
       {/* MESSAGE Display */}
+
+      {message &&(
+        <div className='absolute left-2 top-5 rounded-3xl px-4 py-2' style={{ backgroundColor: color }}>
+          <p className='text-white whitespace-nowrap text-sm leading-relaxed'>{message}</p>
+        </div>
+      )}
     </div>
   )
 }
